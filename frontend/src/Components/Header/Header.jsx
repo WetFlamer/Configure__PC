e161ea398222c01f1387a5ae769674b731d63d49
WetFlamer <xamidchabaev04@gmail.com>
2022-12-28 16:48:37 +0300

    Header v1.0

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -2,8 +2,40 @@ import React from "react";
 import styles from "./Header.module.css";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+const [style, setStyles] = useState(styles.navigationButtonSelect)
+const [style2, setStyles2] = useState(styles.navigationButton)
+const [style3, setStyles3] = useState(styles.navigationButton)
+const [style4, setStyles4] = useState(styles.navigationButton)
+
+const handleStyleSelect = () => {
+    setStyles(styles.navigationButtonSelect)
+    setStyles2(styles.navigationButton)
+    setStyles3(styles.navigationButton)
+    setStyles4(styles.navigationButton)
+}
+const handleStyleSelect2 = () => {
+  setStyles(styles.navigationButton)
+  setStyles2(styles.navigationButtonSelect)
+  setStyles3(styles.navigationButton)
+  setStyles4(styles.navigationButton)
+}
+const handleStyleSelect3 = () => {
+  setStyles(styles.navigationButton)
+  setStyles2(styles.navigationButton)
+  setStyles3(styles.navigationButtonSelect)
+  setStyles4(styles.navigationButton)
+}
+const handleStyleSelect4 = () => {
+  setStyles(styles.navigationButton)
+  setStyles2(styles.navigationButton)
+  setStyles3(styles.navigationButton)
+  setStyles4(styles.navigationButtonSelect)
+}
+
+
   return (
     <div className={styles.header}>
       <div className={styles.logoBlock}>
@@ -11,18 +43,21 @@ const Header = () => {
         <div className={styles.logoText}>ATRIX</div>
       </div>
       <div className={styles.navigationBlock}>
-      <Link className={styles.navigationButtonSelect} to="/">
+      <Link onClick={handleStyleSelect} className={style} to="/">
           Главная
         </Link>
-        <Link className={styles.navigationButton} to="/configure">
+        <Link onClick={handleStyleSelect2} className={style2} to="/configure">
           Собрать
         </Link>
-        <Link className={styles.navigationButton} to="/assembly">
+        <Link onClick={handleStyleSelect3} className={style3} to="/assembly">
           Сборки
         </Link>
-        <Link className={styles.navigationButton} to="/about">
+        <Link onClick={handleStyleSelect4} className={style4} to="/about">
           О нас
         </Link>
+      </div>
+      <div className={styles.loginBlock}>
+        <button className={styles.loginButton}>Войти</button>
       </div>
     </div>
   );
