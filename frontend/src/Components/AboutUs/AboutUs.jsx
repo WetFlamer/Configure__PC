ad669446c39038cc9f1f854f8c54aa22265db90d
WetFlamer <xamidchabaev04@gmail.com>
2022-12-29 13:42:59 +0300

    Добавлен скелет авторизации

--- frontend/src/Components/AboutUs/AboutUs.jsx
+++ frontend/src/Components/AboutUs/AboutUs.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import styles from "./AboutUs.module.css";
+import Contacts from "./Contacts";
+import History from "./History";
 
 const AboutUs = () => {
   return (
     <div className={styles.about}>
-      <div className={styles.aboutBlock}>
-        <div className={styles.title}>
-          MATRIX – один из лидеров рынка по сборке цифровой технике в России
-        </div>
-        <p className={styles.description}>
-          Наша цель изменить жизнь людей, сделав простым доступ к огромному
-          количеству качественных и недорогих товаров, предоставляя лучший
-          сервис.
-        </p>
-        <h3 className={styles.titleHistory}>Наша история</h3>
-        <div className={styles.history1}>
-        
-    </div>
-      </div>
-    
+      <Contacts />
+      <History/>
     </div>
   );
 };
