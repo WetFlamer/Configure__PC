f4924a22c4e588ac4b56f4335043fa55c2e3db47
DexterYBN <yakubov959@mail.ru>
2022-12-29 11:03:32 +0300

    сборки бек

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,6 @@ const router = Router();
 router.use("/", require("./Users.route"));
 router.use("/", require("./Categories.route"));
 router.use("/", require("./Accessories.route"));
+router.use("/", require("./Assembly.route"));
 
 module.exports = router;
