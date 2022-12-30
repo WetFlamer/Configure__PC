615de905f11f0259bb544f5bfa2c70262167af20
WetFlamer <xamidchabaev04@gmail.com>
2022-12-30 14:03:38 +0300

    Полностью реализована авторизация

--- frontend/src/features/usersSlice.js
+++ frontend/src/features/usersSlice.js
@@ -2,13 +2,14 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const initialState = {
   loading: false,
   error: false,
+  successfully: null,
   users: [],
-  token: localStorage.getItem("token")
+  token: localStorage.getItem("token"),
 };
 
 export const authSignIn = createAsyncThunk(
   "auth/signIn",
-  async ({login, password}, thunkAPI) => {
+  async ({ login, password }, thunkAPI) => {
     try {
       const res = await fetch("/login", {
         method: "POST",
@@ -17,11 +18,31 @@ export const authSignIn = createAsyncThunk(
       });
       const user = await res.json();
 
-      if(user.error) {
-     return thunkAPI.rejectWithValue(user.error); 
+      if (user.error) {
+        return thunkAPI.rejectWithValue(user.error);
+      }
+      localStorage.setItem("token", user.token);
+      return thunkAPI.fulfillWithValue(user);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
-    localStorage.setItem('token', user.token)
-    return thunkAPI.fulfillWithValue(user);
+  }
+);
+export const authSignUp = createAsyncThunk(
+  "auth/signUp",
+  async ({ login, password }, thunkAPI) => {
+    try {
+      const res = await fetch("/registration", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ login, password }),
+      });
+      const user = await res.json();
+
+      if (user.error) {
+        return thunkAPI.rejectWithValue(user.error);
+      }
+      return thunkAPI.fulfillWithValue(user);
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -33,19 +54,36 @@ const usersSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder 
-    .addCase(authSignIn.pending, (state) => {
+    builder
+      .addCase(authSignIn.pending, (state) => {
         state.loading = true;
         state.error = false;
-    })
-    .addCase(authSignIn.rejected, (state, action) => {
+      })
+      .addCase(authSignIn.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
-    })
-    .addCase(authSignIn.fulfilled, (state, action) => {
+      })
+      .addCase(authSignIn.fulfilled, (state, action) => {
         state.loading = false;
         state.error = false;
-    })
+      })
+
+      // REGISTER
+      .addCase(authSignUp.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+        state.successfully = null;
+      })
+      .addCase(authSignUp.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.successfully = null;
+      })
+      .addCase(authSignUp.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = false;
+        state.successfully = action.payload;
+      });
   },
 });
 
