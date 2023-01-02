228376ca2d47a5dcb3297d6665707094a8dea179
WetFlamer <xamidchabaev04@gmail.com>
2023-01-02 21:27:46 +0300

    Добавлены новые сборки, поправлены стили под новые сборки, исправлены мелкие детали

--- frontend/src/Components/Assembly/Product/Parts.jsx
+++ frontend/src/Components/Assembly/Product/Parts.jsx
@@ -14,59 +14,145 @@ const Parts = ({
   const accessory = useSelector((state) => state.accessories.acs);
   return (
     <div>
-     <div>
-     <p className={styles.acsTitle}>Видео-карта:</p>
-      {accessory.map((item) => {
-        if (item._id === graphics) {
-          return <div className={styles.acsBlockN}> <img className={styles.posterAcs} src={`/assets/images/${item.category.image_white}`} alt="" /><p className={styles.acsName}>{item.name}</p></div>
-        }
-        return null;
-      })}
-     </div>
+      <div>
+        <p className={styles.acsTitle}>Видео-карта:</p>
+        {accessory.map((item) => {
+          if (item._id === graphics) {
+            return (
+              <div className={styles.acsBlockN}>
+                {" "}
+                <img
+                  className={styles.posterAcs}
+                  src={`/assets/images/${item.category.image_white}`}
+                  alt=""
+                />
+                <p className={styles.acsName}>{item.name}</p>
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
       <p className={styles.acsTitle}>Процессор:</p>
       {accessory.map((item) => {
         if (item._id === processor) {
-          return <div className={styles.acsBlockN}> <img className={styles.posterAcs} src={`/assets/images/${item.category.image_white}`} alt="" /><p className={styles.acsName}>{item.name}</p></div>
+          return (
+            <div className={styles.acsBlockN}>
+              {" "}
+              <img
+                className={styles.posterAcs}
+                src={`/assets/images/${item.category.image_white}`}
+                alt=""
+              />
+              <p className={styles.acsName}>{item.name}</p>
+            </div>
+          );
         }
         return null;
       })}
       <p className={styles.acsTitle}>Оперативная память:</p>
       {accessory.map((item) => {
         if (item._id === ram) {
-          return <div className={styles.acsBlockN}> <img className={styles.posterAcs} src={`/assets/images/${item.category.image_white}`} alt="" /><p className={styles.acsName}>{item.name}</p></div>
+          return (
+            <div className={styles.acsBlockN}>
+              {" "}
+              <img
+                className={styles.posterAcs}
+                src={`/assets/images/${item.category.image_white}`}
+                alt=""
+              />
+              <p className={styles.acsName}>{item.name}</p>
+            </div>
+          );
         }
         return null;
       })}
       <p className={styles.acsTitle}>Материнская плата:</p>
       {accessory.map((item) => {
         if (item._id === motherboard) {
-          return <div className={styles.acsBlockN}> <img className={styles.posterAcs} src={`/assets/images/${item.category.image_white}`} alt="" /><p className={styles.acsName}>{item.name}</p></div>
+          return (
+            <div className={styles.acsBlockN}>
+              {" "}
+              <img
+                className={styles.posterAcs}
+                src={`/assets/images/${item.category.image_white}`}
+                alt=""
+              />
+              <p className={styles.acsName}>{item.name}</p>
+            </div>
+          );
         }
         return null;
       })}
-      {ssd ? <p className={styles.acsTitle}>SSD:</p> : <p className={styles.acsTitle}>HDD:</p>}
-      {accessory.map((item) => {
-
-        if (item._id === ssd) {
-          return <div className={styles.acsBlockN}> <img className={styles.posterAcs} src={`/assets/images/${item.category.image_white}`} alt="" /><p className={styles.acsName}>{item.name}</p></div>
-        }
-        if(item._id === hdd) {
-          return <div className={styles.acsBlockN}> <img className={styles.posterAcs} src={`/assets/images/${item.category.image_white}`} alt="" /><p className={styles.acsName}>{item.name}</p></div>
-
-        } 
-        return null;
-      })}
+      {ssd ? <p className={styles.acsTitle}>SSD:</p> : null}
+      {ssd
+        ? accessory.map((item) => {
+            if (item._id === ssd) {
+              return (
+                <div className={styles.acsBlockN}>
+                  {" "}
+                  <img
+                    className={styles.posterAcs}
+                    src={`/assets/images/${item.category.image_white}`}
+                    alt=""
+                  />
+                  <p className={styles.acsName}>{item.name}</p>
+                </div>
+              );
+            }
+            return null;
+          })
+        : null}
+      {hdd ? <p className={styles.acsTitle}>HDD:</p> : null}
+      {hdd
+        ? accessory.map((item) => {
+            if (item._id === hdd) {
+              return (
+                <div className={styles.acsBlockN}>
+                  {" "}
+                  <img
+                    className={styles.posterAcs}
+                    src={`/assets/images/${item.category.image_white}`}
+                    alt=""
+                  />
+                  <p className={styles.acsName}>{item.name}</p>
+                </div>
+              );
+            }
+            return null;
+          })
+        : null}
       <p className={styles.acsTitle}>Блок питания:</p>
       {accessory.map((item) => {
         if (item._id === power) {
-          return <div className={styles.acsBlockN}> <img className={styles.posterAcs} src={`/assets/images/${item.category.image_white}`} alt="" /><p className={styles.acsName}>{item.name}</p></div>
+          return (
+            <div className={styles.acsBlockN}>
+              {" "}
+              <img
+                className={styles.posterAcs}
+                src={`/assets/images/${item.category.image_white}`}
+                alt=""
+              />
+              <p className={styles.acsName}>{item.name}</p>
+            </div>
+          );
         }
         return null;
       })}
       <p className={styles.acsTitle}>Корпус:</p>
       {accessory.map((item) => {
         if (item._id === compCase) {
-          return <div className={styles.acsBlockN}> <img className={styles.posterAcs} src={`/assets/images/${item.category.image_white}`} alt="" /><p className={styles.acsName}>{item.name}</p></div>
+          return (
+            <div className={styles.acsBlockN}>
+              {" "}
+              <img
+                className={styles.posterAcs}
+                src={`/assets/images/${item.category.image_white}`}
+                alt=""
+              />
+              <p className={styles.acsName}>{item.name}</p>
+            </div>
+          );
         }
         return null;
       })}
