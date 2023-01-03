a9171caacbb5bac041cb7e1aadd15f68e94210ab
WetFlamer <xamidchabaev04@gmail.com>
2023-01-03 16:48:33 +0300

    добавлен блок в assembler

--- frontend/src/Components/Assembler/Assembler.jsx
+++ frontend/src/Components/Assembler/Assembler.jsx
@@ -4,17 +4,21 @@ import styles from "./Assembler.module.css";
 import Accessories from "./Accessories/Accessories";
 import Detail from "./Detail/Detail";
 import Total from "./Total/Total";
+import Main from "./Main/Main";
 
 const Assembler = () => {
   return (
+    <>
+      <Main/>
     <div className={styles.main}>
+      
       <div className={styles.bg}>
         <Categories />
         <Accessories />
         <Detail />
         <Total />
       </div>
-    </div>
+    </div></>
   );
 };
 
