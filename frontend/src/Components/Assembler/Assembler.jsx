5550032a4c0e2cae16e8d8ff5d12d4a0b7fd629e
DexterYBN <yakubov959@mail.ru>
2023-01-03 14:26:16 +0300

    общая цена

--- frontend/src/Components/Assembler/Assembler.jsx
+++ frontend/src/Components/Assembler/Assembler.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Categories from "./Categories/Categories";
-import styles from './Assembler.module.css';
+import styles from "./Assembler.module.css";
 import Accessories from "./Accessories/Accessories";
 import Detail from "./Detail/Detail";
+import Total from "./Total/Total";
 
 const Assembler = () => {
-  
   return (
     <div className={styles.main}>
       <div className={styles.bg}>
         <Categories />
-        <Accessories/>
+        <Accessories />
         <Detail />
+        <Total />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Assembler;
